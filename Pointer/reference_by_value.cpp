94fb8518d464ffc5ec0f8b70f0a2eeeaa10af379
saif-dev-19 <anonymous.safu@gmail.com>
2026-07-11 23:51:41 +0600

    reference by variable

--- Pointer/reference_by_value.cpp
+++ Pointer/reference_by_value.cpp
@@ -1,15 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+// Reference by variable
+
+
+void changeA(int &a){
+    a = 20;
+    cout << a << endl;
+}
+
 int main(){
     int a = 10;
-    int &b = a; // b is a reference variable that refers to a
+    // int &b = a; // b is a reference variable that refers to a
 
-    b = 20; // changing the value of b will also change the value of a
+    // b = 20; // changing the value of b will also change the value of a
 
 
-    cout << &b << endl;
-    cout << "The value of b is: " << b << endl;
-    cout << "The value of a is: " << a << endl;
+    // cout << &b << endl;
+    // cout << "The value of b is: " << b << endl;
+    // cout << "The value of a is: " << a << endl;
+
+    changeA(a);
+    cout << a << endl;
 
 }
