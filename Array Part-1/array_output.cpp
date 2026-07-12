7a9f054a5ac5a9a273721e22e3c8e8105854e6bb
saif-dev-19 <anonymous.safu@gmail.com>
2026-07-12 23:12:15 +0600

    Array output with length

---
+++ Array Part-1/array_output.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[25] = {2,3,5,1,6,7,8,9};
+    int n = sizeof(arr)/sizeof(int);
+
+    for(int i =0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+}
