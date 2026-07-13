1d1a6ae4fff69f48fdbef526a73de2984eb46553
saif-dev-19 <anonymous.safu@gmail.com>
2026-07-14 00:38:27 +0600

    Reverse array without extra spacing

---
+++ Array Part-1/reverse_array_with_two_pointer.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Not extra variable
+void swap(int arr[],int start,int end){
+    arr[start] = arr[start] + arr[end];
+    arr[end] = arr[start] - arr[end];
+    arr[start] = arr[start] - arr[end];
+}
+
+//With extra temp variable
+// void swap(int arr[],int start,int end){
+//     int temp = arr[start];
+//     arr[start] = arr[end];
+//     arr[end] = temp;
+// }
+
+int main(){
+
+    int arr[]= {5,4,2,1,3};
+
+    int n = sizeof(arr)/sizeof(int);
+
+    int start = 0; int end = n-1;
+
+    while(start < end){
+        swap(arr,start,end);
+        start++;
+        end--;
+    }
+
+    for(int i =0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
+}
