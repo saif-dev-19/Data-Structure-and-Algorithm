2d6cc1a908c797141d68c780a8abafff045db447
saif-dev-19 <anonymous.safu@gmail.com>
2026-07-13 23:57:21 +0600

    Linear Search

---
+++ Array Part-1/linear_search.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void linear_search(int arr[], int n, int key){
+    for(int i = 0; i<n; i++){
+        if (arr[i] == key){
+            cout << "Element found at index: " << i << endl;
+            return;
+        }
+    }
+    cout << "Element not found" << endl;
+}
+
+
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    linear_search(arr, n, key);
+
+}
