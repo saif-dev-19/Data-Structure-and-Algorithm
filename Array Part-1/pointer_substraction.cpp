65cdd2507cedad2c229445b15ce8efb1501a713e
saif-dev-19 <anonymous.safu@gmail.com>
2026-08-18 00:51:29 +0600

    Pointer Substruction

---
+++ Array Part-1/pointer_substraction.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int arr[] = {2,4,6,8,9,10,12,14,15};
+
+    int *ptr1 = arr; //2
+    int *ptr2 = ptr1 + 3; //8
+
+    cout << *ptr1 << endl;
+    cout << *ptr2 << endl;
+
+    cout << ptr2 - ptr1 << endl;
+
+
+
+    
+}
