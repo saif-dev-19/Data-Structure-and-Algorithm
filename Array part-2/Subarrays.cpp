28001227f5a947cbc528a06048b6a81a0633be0d
saif-dev-19 <anonymous.safu@gmail.com>
2026-08-18 20:34:00 +0600

    change

--- Array part-2/Subarrays.cpp
+++ Array part-2/Subarrays.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void printSubarrays(int arr[], int n){
     for(int start = 0; start<n; start++){
         for(int end = start; end<n; end++){
+            // cout << "(" << start << "," << end << ") ";
             cout << "(";
             for(int i = start; i<=end; i++){
                 cout <<arr[i];
