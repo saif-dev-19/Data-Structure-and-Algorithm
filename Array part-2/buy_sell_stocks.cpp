f4d62faab252cd7051942ddb122eceb055d99b9b
saif-dev-19 <anonymous.safu@gmail.com>
2026-08-20 06:15:05 +0600

    Buy sell stock max profit

---
+++ Array part-2/buy_sell_stocks.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void maxProfit(int prices[], int n){
+    int bestBuy[10000];
+    bestBuy[0] = INT_MAX;
+
+    for(int i =1; i<n; i++){
+        bestBuy[i] = min(bestBuy[i-1],prices[i-1]);
+    }
+
+    int maxProfit = 0;
+
+    for(int i =0; i<n; i++){
+        int currentProfit = prices[i] - bestBuy[i];
+        maxProfit = max(currentProfit,maxProfit);
+    }
+
+    cout << maxProfit;
+}
+
+int main(){
+    int arr[] = {7,1,5,3,9,4};
+    int n = 5;
+
+    maxProfit(arr,n);
+}
